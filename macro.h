e1901a2069f37d7ecb3519b575c47fd5d2b5fb94
max <maksymilian.babarowski@gmail.com>
2018-11-13 20:25:02 +0100

    Functions checking if input is correct

---
+++ macro.h
@@ -0,0 +1,19 @@
+#ifndef MACRO_H
+#define MACRO_
+
+// Aliases for J-A ranks
+#define J 11
+#define Q 12
+#define K 13
+#define A 14
+
+// Aliases for suits
+#define D 1
+#define C 2
+#define H 3
+#define S 4
+
+int isCorrectRank( const char );
+int isCorrectSuit( const char );
+
+#endif // MACRO_H
